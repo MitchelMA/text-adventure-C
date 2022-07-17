b81a378bc90e390360bb434a2c242700d102e1f0
MitchelMA <32548@ma-web.nl>
2022-07-17 18:38:01 +0200

    bezig met duidelijke comments voor functions

--- Adventure/adventure.h
+++ Adventure/adventure.h
@@ -2,12 +2,51 @@
 #define ADVENTURE_HEADER
 #include "../Structs/Structs.h"
 
+/**
+ * @brief Creates a new instance of a Scene structure
+ *
+ * @param text The text that will be displayed when the Scene is initialized
+ * @return The new instance of a Scene Structure
+ */
 Scene *newScene(char *text);
+
+/**
+ * @brief outputs the text and the options of the scene
+ *
+ * @param scene The scene whose text and options will be displayed
+ */
 void initScene(Scene *scene);
+
+/**
+ * @brief Handles the input of the player correctly (e.g. checking for NULL values)
+ *
+ * @param scene The scene whose Options will be checked
+ * @param input The input value as a number
+ * @return The chosen option by the user
+ */
 Option *handleInput(Scene *scene, int input);
 
+/**
+ * @brief Creates a new instance of an Option Structure
+ *
+ * @param optionText The text that will be displayed when the scene that contains this option is initialized
+ * @param get The item that the player will get in their inventory by choosing this option
+ * @param need The item that the player will need to be able to choose this option
+ * @param nextScene A reference to the next scene
+ * @param handler A Function Pointer that will handle the input of the player
+ * @return The new instance of an Option Structure
+ */
 Option *newOption(char *optionText, char *get, char *need, Scene *nextScene, Scene *(*handler)(Scene *, Option *, char **, int));
 
+/**
+ * @brief The basic option-handle function
+ *
+ * @param currentScene A reference to the current scene
+ * @param chosenOption A reference to the chosen option
+ * @param inventory The inventory of the player
+ * @param inventorySize The size of the inventory of the player
+ * @return The reference to the next scene
+ */
 Scene *basicHandler(Scene *currentScene, Option *chosenOption, char **inventory, int inventorySize);
 
 #endif
--- Structs/Structs.h
+++ Structs/Structs.h
@@ -3,14 +3,35 @@
 
 #pragma region ADVENTURE
 
+/**
+ * @brief Structure that contains all the info about a scene
+ *
+ */
 typedef struct Scene Scene;
+
+/**
+ * @brief Structure that contains all the info about an option in a scene
+ *
+ */
 typedef struct Option Option;
 
 #pragma endregion ADVENTURE
 
 #pragma region LINKDEDLIST
 
+/**
+ * @brief Structure that contians the head and size. @n
+ * A linked list can increase and decrease in size at will.
+ *
+ */
 typedef struct LinkedList LinkedList;
+
+/**
+ * @brief Structure that is part of a LinkedList. @n
+ * A LinkedListNode contains (in this situation) an Option for a Scene and @n
+ * a reference to the next element in the list.
+ *
+ */
 typedef struct LinkedListNode LinkedListNode;
 
 #pragma endregion LINKEDLIST
